42aa103d962b624a9ae647adb911a1d417985614
hyunwoo <h.hyunwoo@gmail.com>
2019-06-25 18:13:12 +0900

    add performance

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/storage';
 import 'firebase/firestore';
+import 'firebase/performance';
 
 import config from '@/firebaseConfig';
 
@@ -10,9 +11,10 @@ if (app === null) {
   app = firebase.initializeApp(config);
 }
 
+const perf = app.performance();
 const database = app.firestore();
 const storage = app.storage();
 const auth: firebase.auth.Auth = app.auth();
 
 export default app;
-export { database, storage, auth };
+export { database, storage, auth, perf };
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { FirestoreCollection, FirestoreDocument, FirestoreDocumentData } from './firestore';
 import Storage from './storage';
 import Auth from './auth';
-
-export { FirestoreCollection, FirestoreDocument, Storage, Auth, FirestoreDocumentData };
+import { perf as Performance } from './app';
+export { FirestoreCollection, FirestoreDocument, Storage, Auth, FirestoreDocumentData, Performance };
